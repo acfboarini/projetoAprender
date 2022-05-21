4a0733babbe6f5591f0dde4b2b9d9b566a56c35a
Ceso Tarde <cesotardesedhas@gmail.com>
2022-05-20 21:14:49 -0300

    add Login Cadastrar

--- projetoAprender-front/aprender/src/Rotas.jsx
+++ projetoAprender-front/aprender/src/Rotas.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { AprenderProvider } from "./Contexts/index"
 import Login from "./Pages/Login"
+import Cadastrar from "./Pages/Cadastrar"
+import Desempenho from "./Pages/Desempenho"
 
 export default function Router() {
     return(
@@ -8,6 +10,8 @@ export default function Router() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Login/>} />
+                    <Route path="/cadastrar" element={<Cadastrar/>} />
+                    <Route path="/desempenho" element={<Desempenho/>} />
                 </Routes>
             </BrowserRouter>
         </AprenderProvider>
